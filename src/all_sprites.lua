3e437a343e42a8150a54264f6bd9d1b787761b29
Paul Parisot <paul.parisot@epitech.eu>
2023-05-27 21:12:11 +0200

    Fix(sprites): Better sprite creation

--- src/all_sprites.lua
+++ src/all_sprites.lua
@@ -5,7 +5,7 @@ local all_sprites = {}
 
 function all_sprites:Create(image, pos, scale, rotation)
     local sprite = {}
-    sprite.image = image
+    sprite.image = love.graphics.newImage(image)
     sprite.quad = love.graphics.newQuad(
         0, 0, sprite.image:getWidth(), sprite.image:getHeight(), sprite.image:getDimensions()
     )
@@ -18,12 +18,7 @@ end
 
 all_sprites.septup = function ()
     
-    _G.player = all_sprites:Create(
-        love.graphics.newImage("assets/perso.png"),
-        Vector.new(100, 100),
-        .3,
-        0
-    )
+    _G.player = all_sprites:Create("assets/perso.png", Vector.new(100, 100), .3, 0)
 
     --> table.insert(_G.scenes.Game.sprites, all_sprites:Create(image, pos (Vector.new), scale, rotation))
 
