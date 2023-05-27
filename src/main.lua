12f46d7c304ba87e7c55c43c2640d79363cc1325
Paul Parisot <paul.parisot@epitech.eu>
2023-05-27 16:57:32 +0200

    Feat(exec, sprite): Add the exec file + sprite system
    - Execute and download dependencies
    - Install love in a boot
    - Create sprite

--- src/main.lua
+++ src/main.lua
@@ -2,16 +2,26 @@ local input     = require('./input')
 local my        = require('./my')
 local timer     = require('./timer')
 
+local sprites = {}
+local workdir = love.filesystem.getWorkingDirectory()
+
 love.load = function()
     love.graphics.setBackgroundColor(255, 255, 255)
 
-    timer:Create("hello", .5, function ()
+    timer:Create("hello", 1, function()
         print("hello")
     end)
+
+    local sprite = {}
+    sprite.image = love.graphics.newImage("assets/perso.png")
+    sprite.quad = love.graphics.newQuad(0, 0, sprite.image:getWidth(), sprite.image:getHeight(), sprite.image:getDimensions())
+    table.insert(sprites, sprite)
 end
 
 love.draw = function ()
-    
+    for _, sprite in pairs(sprites) do
+        love.graphics.draw(sprite.image, sprite.quad, 100, 100)
+    end
 end
 
 love.update = function (dt)
