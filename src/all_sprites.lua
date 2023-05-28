7852243b3e0dedd5e2d675912bc55a2385657c71
username <damian.gil@epitech.eu>
2023-05-28 14:26:20 +0200

    feat: new white enemy
    - doesn't follow for now

--- src/all_sprites.lua
+++ src/all_sprites.lua
@@ -3,28 +3,32 @@ local Vector    = require('./vector')
 
 local all_sprites = {}
 
-function all_sprites:Create(image, pos, scale, rotation)
+function all_sprites:Create(image, pos, scale, rotation, color)
     local sprite = {}
-    sprite.image = love.graphics.newImage(image)
+    image = (color and color == "white") and (image .. "_2") or image
+    sprite.image = love.graphics.newImage(image .. ".png")
     sprite.quad = love.graphics.newQuad(
         0, 0, sprite.image:getWidth(), sprite.image:getHeight(), sprite.image:getDimensions()
     )
     sprite.pos = pos or Vector.new(0, 0)
     sprite.scale = scale or 1
     sprite.rotation = rotation or 0
+    sprite.color = color or "white"
     return sprite
 end
 
 all_sprites.septup = function ()
     
-    _G.player = all_sprites:Create("assets/perso.png", Vector.new(100, 100), .3, 0)
+    _G.player = all_sprites:Create("assets/perso", Vector.new(100, 100), .3, 0)
 
-    table.insert(_G.scenes.Game.sprites, all_sprites:Create("assets/ennemi.png", Vector.new(50, 300), .25, 0))
+    table.insert(_G.scenes.Game.sprites, all_sprites:Create("assets/ennemi", Vector.new(50, 300), .25, 0, "black"))
+    table.insert(_G.scenes.Game.sprites, all_sprites:Create("assets/ennemi", Vector.new(50, 300), .25, 0, "white"))
 
     return _G.scenes
 end
 
 function all_sprites:updateEnemy(enemy, player, dt)
+    if _G.player.color == enemy.color then return end
     local direction = Vector.sub(player.pos, enemy.pos)
     direction = Vector.normalize(direction)
     local speed = 75
